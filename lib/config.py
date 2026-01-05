436eb19e91a41e53f76406e74e3bc213d184e5d4
matiasprietohernan <matiasprietohernan@gmail.com>
2026-01-05 11:24:55 -0300

    Refactor docker-compose.yml and config.py for improved Qdrant configuration and health checks

--- lib/config.py
+++ lib/config.py
@@ -7,13 +7,12 @@ import os
 load_dotenv()
 
 config = {
-    "qdrant_url": os.getenv("QDRANT_URL"),
-    "qdrant_key": os.getenv("QDRANT_API_KEY"),
     "embedding_model": os.getenv("MODEL_EMBEDDING", "text-embedding-3-small"),
-    "collection": os.getenv("COLLECTION_NAME", "HogarFe"),
-    "api_key": os.getenv("API_KEY")
+    "api_key": os.getenv("API_KEY"),
+    "qdrant_host": os.getenv("QDRANT_HOST", "localhost"),
+    "qdrant_port": os.getenv("QDRANT_PORT", 6333)
 }
 
-client = QdrantClient(url=config["qdrant_url"])
+client = QdrantClient(host=config["qdrant_host"], port=config["qdrant_port"])
 embeddings = OpenAIEmbeddings(model=config["embedding_model"])
 sparse_embeddings = SparseTextEmbedding(model_name="Qdrant/bm25")
